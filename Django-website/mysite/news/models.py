7b5d87dd43a8d4a951434c945687a01d8bc848b4
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2020-12-16 14:07:30 +0300

    исправлено название полей добовление фото опционально

--- Django-website/mysite/news/models.py
+++ Django-website/mysite/news/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d', max_length=250)
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name='Название')
+    content = models.TextField(verbose_name='Содержание новости')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', max_length=250, verbose_name='Фото', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликована')
 
     def __str__(self):
         return self.title
