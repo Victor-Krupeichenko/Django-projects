871deded126a7c4cb2627fd25ad4936898a6b306
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2020-12-16 14:23:30 +0300

    Зарегистрированна модель Категории

--- Django-website/mysite/news/admin.py
+++ Django-website/mysite/news/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import News
+from .models import News, Category
 
 
 
@@ -9,4 +9,10 @@ class NewsAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content')
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
