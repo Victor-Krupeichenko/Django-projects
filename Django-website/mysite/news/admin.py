0eccad0965ffaf990e4bd6010f4fc7152925cda3
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2020-12-16 14:08:59 +0300

    добавлено поля которые будут ссылками на новость

--- Django-website/mysite/news/admin.py
+++ Django-website/mysite/news/admin.py
@@ -5,6 +5,7 @@ from .models import News
 
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('id', 'title')
 
 
 admin.site.register(News, NewsAdmin)
