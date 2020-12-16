fb93a0ca4f9c35ce69a76f5958703eab727c3d7f
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2020-12-16 13:58:37 +0300

    немного поднастроена админка

--- Django-website/mysite/news/models.py
+++ Django-website/mysite/news/models.py
@@ -6,8 +6,14 @@ class News(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', max_length=250)
     is_published = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
+
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at', 'title']
