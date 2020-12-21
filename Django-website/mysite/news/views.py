23049b71844bb7e7aa492ba6335aa3b2b5442ba0
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2020-12-22 01:55:16 +0300

    добавлен переход по категориям

--- Django-website/mysite/news/views.py
+++ Django-website/mysite/news/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import News
-
+from .models import News, Category
 
 def index(request):
-    news = News.objects.order_by('-created_at')
+    news = News.objects.all()
+    categories = Category.objects.all()
     context = {
-        'news': news,
-        'title': 'Список новостей'
+        'news':news,
+        'title':'Список новостей',
+        'categories':categories
     }
     return render(request, 'news/index.html', context)
+
+def get_categories(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news':news,
+        'categories':categories,
+        'category': category
+    }
+    return render(request, 'news/category.html', context)
+
