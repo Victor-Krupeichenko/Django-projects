36aa7a5750032877370634ca33b9c8cc197deeee
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2020-12-22 15:17:57 +0300

    добавлен маршрут для просмотра конкретной новости

--- Django-website/mysite/news/urls.py
+++ Django-website/mysite/news/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('category/<int:category_id>/', get_categories, name='category')
+    path('category/<int:category_id>/', get_categories, name='category'),
+    path('news/<int:news_id>/', view_news, name='view_news'),
+
 ]
