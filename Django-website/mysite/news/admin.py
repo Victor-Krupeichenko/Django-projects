4fba48cacac3a0955417b44998e8d6a7b7f21f55
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2020-12-22 13:58:00 +0300

    предварительная настройка админки

--- Django-website/mysite/news/admin.py
+++ Django-website/mysite/news/admin.py
@@ -1,21 +1,19 @@
 from django.contrib import admin
+
 from .models import News, Category
 
-
-
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category','created_at', 'updated_at', 'is_published')
-    list_display_links = ('id', 'title')
-    search_fields = ('title', 'content')
+    list_display = ('title', 'category', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('title', 'category')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'category')
-
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title')
-    list_display_links = ('id', 'title')
     search_fields = ('title',)
 
+
+class NewsCategory(admin.ModelAdmin):
+    list_display = ('title', )
+    list_display_links = ('title', )
+    search_fields = ('category',)
+
 admin.site.register(News, NewsAdmin)
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(Category, NewsCategory)
