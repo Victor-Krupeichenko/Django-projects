bc8adb587d60272cb4825eba8b397e992c666dcd
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2020-12-25 19:11:27 +0300

    добавлен контроллер формы привязанной к модели для добовления новости

--- Django-website/mysite/news/views.py
+++ Django-website/mysite/news/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import NewsForm
+
 from .models import News, Category
+
 
 def index(request):
     news = News.objects.all()
     context = {
-        'news':news,
-        'title':'Список новостей',
+        'news': news,
+        'title': 'Список новостей',
     }
     return render(request, 'news/index.html', context)
 
 def get_categories(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    category = Category.objects.get(pk=category_id)
+    category = get_object_or_404(Category, pk=category_id)
     context = {
         'news':news,
         'category': category
@@ -19,8 +22,22 @@ def get_categories(request, category_id):
     return render(request, 'news/category.html', context)
 
 def view_news(request, news_id):
-    item = News.objects.get(pk=news_id)
+    item = get_object_or_404(News, pk=news_id)
     context = {
         'item':item
     }
     return render(request, 'news/view_news.html', context)
+
+
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = form.save()
+            return redirect(news)
+    else:
+        form = NewsForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'news/add_news.html', context)
