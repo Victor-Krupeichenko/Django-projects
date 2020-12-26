5686358e97aed76b9e50c137bcd5168271313789
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2020-12-27 02:18:28 +0300

    добавлено поля для загрузки фото

--- Django-website/mysite/news/forms.py
+++ Django-website/mysite/news/forms.py
@@ -3,12 +3,14 @@ from django import forms
 from .models import News
 
 
+
 class NewsForm(forms.ModelForm):
     class Meta:
         model = News
-        fields = ['title', 'content', 'is_published', 'category']
+        fields = ['title', 'content','photo','is_published', 'category']
         widgets = {
-            'title':forms.TextInput(attrs={'class':'form-control'}),
-            'content':forms.Textarea(attrs={'class':'form-control', 'rows':5}),
-            'category': forms.Select(attrs={'class':'form-control'})
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+
         }
