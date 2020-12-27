4fb885103a5b42df9e26b825b2b23d80932c702b
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2020-12-27 13:49:50 +0300

    добавлен класс маршрутизатор для просмотра списка новостей

--- Django-website/mysite/news/views.py
+++ Django-website/mysite/news/views.py
@@ -1,44 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import NewsForm
 from .models import News, Category
+from django.views.generic import ListView
 
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/index.html'
+    context_object_name = 'news'
 
-def index(request):
-    news = News.objects.all()
-    context = {
-        'news': news,
-        'title': 'Список новостей',
-    }
-    return render(request, 'news/index.html', context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
 
-
-def get_categories(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    category = get_object_or_404(Category, pk=category_id)
-    context = {
-        'news': news,
-        'category': category
-    }
-    return render(request, 'news/category.html', context)
-
-
-def view_news(request, news_id):
-    item = get_object_or_404(News, pk=news_id)
-    context = {
-        'item': item
-    }
-    return render(request, 'news/view_news.html', context)
-
-
-def add_news(request):
-    if request.method =='POST':
-        form = NewsForm(request.POST, request.FILES)
-        if form.is_valid():
-            news = form.save()
-            return redirect(news)
-    else:
-        form = NewsForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'news/add_news.html', context)
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
