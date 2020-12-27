ec1d577f6ec73f264607c9dacfd7435965019aed
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2020-12-27 14:04:19 +0300

    исправлен маршрут к шаблону формы для добавления новости

--- Django-website/mysite/news/urls.py
+++ Django-website/mysite/news/urls.py
@@ -5,7 +5,6 @@ from .views import *
 urlpatterns = [
     path('', HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/',NewsCategory.as_view(), name='category'),
-    # path('news/<int:news_id>/', name='view_news'),
-    # path('news/add-news/', name='add_news')
-#
+    path('news/add-news/',CreateNews.as_view(), name='add_news')
+
 ]
