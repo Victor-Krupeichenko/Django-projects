cbb6da047a53bbd983c33bb0c2aa00ada24dcd65
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2020-12-27 13:50:42 +0300

    исправлен маршрут для главной страницы

--- Django-website/mysite/news/urls.py
+++ Django-website/mysite/news/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('category/<int:category_id>/', get_categories, name='category'),
-    path('news/<int:news_id>/', view_news, name='view_news'),
-    path('news/add-news/', add_news, name='add_news'),
+    path('', HomeNews.as_view(), name='home'),
+    # path('category/<int:category_id>/', name='category'),
+    # path('news/<int:news_id>/', name='view_news'),
+    # path('news/add-news/', name='add_news')
+#
 ]
