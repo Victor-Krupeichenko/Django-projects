105e4276c387202e5ca91680ea20ade11160074b
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2020-12-30 14:38:24 +0300

    Непоказывать категории у которох нет записей

--- Django-website/mysite/news/templatetags/news_tags.py
+++ Django-website/mysite/news/templatetags/news_tags.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 from django import template
+from django.db.models import Count
+
 from news.models import Category
 
 register = template.Library()
 
-
 @register.inclusion_tag('news/list_categories.html')
 def show_categories():
-    categories = Category.objects.all()
-    context = {
-        'categories':categories
-    }
-    return context
+    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
+    return {'categories':categories}
