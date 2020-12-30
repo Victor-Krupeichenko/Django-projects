5938ea5670c7728a1c91354305cafff091067c2b
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2020-12-30 14:52:40 +0300

    добавлено поля просмотров

--- Django-website/mysite/news/models.py
+++ Django-website/mysite/news/models.py
@@ -2,35 +2,45 @@ from django.db import models
 from django.urls import reverse_lazy
 
 class News(models.Model):
-    title = models.CharField(max_length=150, db_index=True, verbose_name='Название')
-    content = models.TextField(db_index=True, verbose_name='содержание')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='дата обновления')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='фото')
+    title = models.CharField(db_index=True, max_length=150, verbose_name='Название')
+    content = models.TextField(db_index=True, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(blank=True, upload_to='photos/%Y/%m/%d/', verbose_name='фото')
     is_published = models.BooleanField(default=True, verbose_name='опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+    views = models.IntegerField(default=0, verbose_name='просмотры')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Новость'
-        verbose_name_plural = 'Новости'
-        ordering = ['-created_at', 'updated_at', 'title']
+        verbose_name = 'новость'
+        verbose_name_plural = 'новости'
+        ordering = ['-created_at']
 
     def get_absolute_url(self):
-        return reverse_lazy('view_news', kwargs={'news_id':self.pk})
+        context = {
+            'pk': self.pk
+        }
+        return reverse_lazy('view_news', kwargs=context)
+
+
 
 class Category(models.Model):
-    title = models.CharField(max_length=150, verbose_name='Категория', db_index=True)
+    title = models.CharField(db_index=True, max_length=150, verbose_name='Категория')
 
     def __str__(self):
         return self.title
 
+
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        ordering = ['title']
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
 
     def get_absolute_url(self):
-        return reverse_lazy('category', kwargs={'category_id':self.pk})
+        context ={
+            'category_id': self.pk
+        }
+        return reverse_lazy('category', kwargs=context)
+
