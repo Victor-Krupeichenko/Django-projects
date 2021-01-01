986fd7ae3d85982d4d8b0fd00b0bbdd111301405
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-01-01 15:26:37 +0300

    закрыт доступ на добавление новости неавторизованным пользователям

--- Django-website/mysite/news/views.py
+++ Django-website/mysite/news/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render, get_object_or_404, redirect
 from .forms import NewsForm
 from .models import News, Category
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class HomeNews(ListView):
+
+class NewsHome(ListView):
     model = News
     template_name = 'news/index.html'
     context_object_name = 'news'
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super(NewsHome, self).get_context_data(**kwargs)
         context['title'] = 'Главная страница'
+        return context
 
     def get_queryset(self):
-        return News.objects.filter(is_published=True)
+        return News.objects.filter(is_published=True).select_related('category')
+
 
 class NewsCategory(ListView):
     model = News
@@ -22,14 +25,25 @@ class NewsCategory(ListView):
     allow_empty = False
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Список категорий'
+        context = super(NewsCategory, self).get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
         return context
 
     def get_queryset(self):
-        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True).select_related('category')
 
 
-class CreateNews(CreateView):
+class ViewNews(DetailView):
+    model = News
+    template_name = 'news/view_news.html'
+    context_object_name = 'item'
+
+class CreateNews(LoginRequiredMixin, CreateView):
     form_class = NewsForm
     template_name = 'news/add_news.html'
+    login_url = '/admin/'
+
+    def get_context_data(self, **kwargs):
+        context = super(CreateNews, self).get_context_data(**kwargs)
+        context['title'] = 'Добавить новость'
+        return context
