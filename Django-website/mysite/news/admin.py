40f1d5f1d90c1576dbb3ab1f3755e9bf4d296fc3
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-01-01 15:19:36 +0300

    вывод фото и дополниетельных полей при переходе внутрь новости

--- Django-website/mysite/news/admin.py
+++ Django-website/mysite/news/admin.py
@@ -3,13 +3,16 @@ from django.utils.safestring import mark_safe
 
 from .models import News, Category
 
+
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'created_at', 'updated_at','is_published', 'get_photo')
+    list_display = ('title', 'category', 'created_at', 'updated_at', 'is_published', 'get_photo')
     list_display_links = ('title', 'category')
     list_filter = ('is_published', 'category')
     search_fields = ('title', 'content')
-    list_editable = ('is_published', )
-    ordering = ('-created_at', )
+    list_editable = ('is_published',)
+    ordering = ('-created_at',)
+    fields = ('title', 'category', 'content', 'photo', 'get_photo', 'is_published', 'views', 'created_at', 'updated_at')
+    readonly_fields = ('get_photo', 'views', 'created_at', 'updated_at')
 
     def get_photo(self, obj):
         if obj.photo:
@@ -22,13 +25,12 @@ class NewsAdmin(admin.ModelAdmin):
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title',)
-    list_display_links = ('title', )
-    search_fields = ('title', )
+    list_display_links = ('title',)
+    search_fields = ('title',)
+
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
 
 admin.site.site_title = 'Упровление новостями'
 admin.site.site_header = 'Управление новостями'
-
-
