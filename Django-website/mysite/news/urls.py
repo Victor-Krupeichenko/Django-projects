390865b8cddd2c690fd8a9ecefbf884bd0f2f63e
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-01-01 15:55:38 +0300

    оправлено название для views главной страницы

--- Django-website/mysite/news/urls.py
+++ Django-website/mysite/news/urls.py
@@ -3,9 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', HomeNews.as_view(), name='home'),
+    path('', NewsHome.as_view(), name='home'),
     path('category/<int:category_id>/', NewsCategory.as_view(), name='category'),
     path('news/<int:pk>/', ViewNews.as_view(), name='view_news'),
     path('news/add-news/', CreateNews.as_view(), name='add_news'),
-
 ]
