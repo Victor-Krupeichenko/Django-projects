be2035b269470319ca52ba81ba3167cdb20a3b84
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-01-01 15:50:41 +0300

    условие при котором будет работать Debug Toolbar

--- Django-website/mysite/mysite/urls.py
+++ Django-website/mysite/mysite/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -21,7 +22,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls'))
+
 ]
 
 if settings.DEBUG:
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
