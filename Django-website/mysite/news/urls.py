3df085b92cb2476c8196fb979e25cadbef06ea77
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-01-05 14:43:41 +0300

    обавлен маршрут для разавторизации пользователя

--- Django-website/mysite/news/urls.py
+++ Django-website/mysite/news/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('logout/', user_logout, name='logout'),
     path('register/', user_register, name='register'),
     path('login/', user_login, name='login'),
     path('', NewsHome.as_view(), name='home'),
