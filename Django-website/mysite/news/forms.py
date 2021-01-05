7ac787c326ef443e04cd37b3b54217bd1f1f9849
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-01-05 14:20:30 +0300

    добавлена форма авторизации

--- Django-website/mysite/news/forms.py
+++ Django-website/mysite/news/forms.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 from .models import News
@@ -29,3 +29,8 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class':'form-control'}))
