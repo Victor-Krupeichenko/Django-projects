c1e0ccb7590f685fe7f9c80b000ca8d606533d91
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-01-05 13:39:41 +0300

    Добавлена форма регистрации пользователя

--- Django-website/mysite/news/forms.py
+++ Django-website/mysite/news/forms.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 from .models import News
 
 
@@ -14,3 +17,15 @@ class NewsForm(forms.ModelForm):
             'category': forms.Select(attrs={'class': 'form-control'}),
 
         }
+
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label='Имя пользователя', help_text='Максимум 150 символов', widget=forms.TextInput(
+        attrs={'class':'form-control'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Подтвердите пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='E-mail', widget=forms.EmailInput(attrs={'class':'form-control'}))
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
