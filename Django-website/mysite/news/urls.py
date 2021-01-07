d072106ce80ac098d4d70b8043888ede0043f4fd
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-01-07 21:22:24 +0300

    закэширована главная страница

--- Django-website/mysite/news/urls.py
+++ Django-website/mysite/news/urls.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.urls import path
 from .views import *
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('user-logout', user_logout, name='user_logout'),
     path('user-login', user_login, name='user_login'),
     path('user-register', user_register, name='user_register'),
-    path('', NewsHome.as_view(), name='home'),
+    path('',cache_page(60) (NewsHome.as_view()), name='home'),
     path('category/<int:category_id>/', NewsCategory.as_view(), name='category'),
     path('news/<int:pk>/', ViewNews.as_view(), name='view_news'),
     path('news/add-news/', CreateNews.as_view(), name='add_news'),
