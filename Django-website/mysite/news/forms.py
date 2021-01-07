da903d3e76f9235420ab5264c0b84988ce44f2aa
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-01-07 13:29:02 +0300

    Добавлена форма для обратной связи по Email

--- Django-website/mysite/news/forms.py
+++ Django-website/mysite/news/forms.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-
 from .models import News
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 
@@ -19,13 +18,13 @@ class NewsForm(forms.ModelForm):
         }
 
 
-
 class UserRegisterForm(UserCreationForm):
-    username = forms.CharField(label='Имя пользователя', help_text='Максимум 150 символов', widget=forms.TextInput(
-        attrs={'class':'form-control'}))
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label='Подтвердите пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(label='E-mail', widget=forms.EmailInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label='Подтвердите пароль', widget=forms.PasswordInput(attrs={
+        'class':'form-control'}))
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
@@ -34,3 +33,8 @@ class UserRegisterForm(UserCreationForm):
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class':'form-control'}))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+
+
+class ContactForm(forms.Form):
+    subject = forms.CharField(label='Тема', widget=forms.TextInput(attrs={'class':'form-control'}))
+    content = forms.CharField(label='Текст', widget=forms.Textarea(attrs={'class':'form-control', 'rows':5}))
