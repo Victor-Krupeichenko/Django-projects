818d4a78e732a886e9374d3056b98c2c1be6c79e
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-01-07 13:32:05 +0300

    добавлен маршрут к форме для отправки Email

--- Django-website/mysite/news/urls.py
+++ Django-website/mysite/news/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('logout/', user_logout, name='logout'),
-    path('register/', user_register, name='register'),
-    path('login/', user_login, name='login'),
+    path('user-logout', user_logout, name='user_logout'),
+    path('user-login', user_login, name='user_login'),
+    path('user-register', user_register, name='user_register'),
     path('', NewsHome.as_view(), name='home'),
     path('category/<int:category_id>/', NewsCategory.as_view(), name='category'),
     path('news/<int:pk>/', ViewNews.as_view(), name='view_news'),
     path('news/add-news/', CreateNews.as_view(), name='add_news'),
+    path('send-mail/', user_contact_mail, name='user_contact_mail'),
 ]
